457b1d1d3e492397f4cc8efdaeb31b6f711f04e8
Evan Burrell <burrelle@oregonstate.edu>
2019-01-07 10:41:47 +0000

    Custom Paths

--- choose-your-own-adventure/cmd/cyoaweb/main.go
+++ choose-your-own-adventure/cmd/cyoaweb/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"text/template"
 
 	adventure "github.com/burrelle/gophercises/choose-your-own-adventure"
 )
@@ -26,7 +28,84 @@ func main() {
 		panic(err)
 	}
 
-	h := adventure.NewHandler(story)
+	tpl := template.Must(template.New("").Parse(StoryTemplate))
+	h := adventure.NewHandler(story,
+		adventure.WithTemplate(tpl),
+		adventure.WithPathFunction(pathFunction),
+	)
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+	mux.Handle("/", adventure.NewHandler(story))
 	fmt.Printf("\nStarting the server at: http://localhost:%d", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 }
+
+func pathFunction(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+	return path[len("/story/"):]
+}
+
+var StoryTemplate = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <section class="page">
+      <h1>{{.Title}}</h1>
+      {{range .Paragraphs}}
+        <p>{{.}}</p>
+      {{end}}
+      <ul>
+      {{range .Options}}
+        <li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+      {{end}}
+      </ul>
+    </section>
+    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FCF6FC;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #797;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: underline;
+        color: #555;
+      }
+      a:active,
+      a:hover {
+        color: #222;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+  </body>
+</html>`
