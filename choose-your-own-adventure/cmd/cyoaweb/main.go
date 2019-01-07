2003a42e430d1a8ad9917b841aae3aec54a228fd
Evan Burrell <burrelle@oregonstate.edu>
2019-01-07 10:19:25 +0000

    Custom templates

--- choose-your-own-adventure/cmd/cyoaweb/main.go
+++ choose-your-own-adventure/cmd/cyoaweb/main.go
@@ -26,7 +26,7 @@ func main() {
 		panic(err)
 	}
 
-	h := adventure.NewHandler(story)
+	h := adventure.NewHandler(story, nil)
 	fmt.Printf("\nStarting the server at: http://localhost:%d", *port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
