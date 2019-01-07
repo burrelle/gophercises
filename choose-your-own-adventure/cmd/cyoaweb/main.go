0f18549a5defa7c641b60fc7969b815bd5d69d05
Evan Burrell <burrelle@oregonstate.edu>
2019-01-07 09:42:35 +0000

    Building the http.Handler

---
+++ choose-your-own-adventure/cmd/cyoaweb/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	adventure "github.com/burrelle/gophercises/choose-your-own-adventure"
+)
+
+func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
+	filename := flag.String("file", "gopher.json", "the JSON file with CYOA story")
+	flag.Parse()
+	fmt.Printf("Using the story in %s", *filename)
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := adventure.JsonStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	h := adventure.NewHandler(story)
+	fmt.Printf("\nStarting the server at: http://localhost:%d", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+}
---
+++ choose-your-own-adventure/story.go
@@ -0,0 +1,72 @@
+package adventure
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"text/template"
+)
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTemplate = `
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta charset="utf-8">
+	<title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+	<h1>{{.Title}}</h1>
+	{{range .Paragraphs}}
+		<p>{{.}}</p>
+	{{end}}
+	<ul>
+	{{range .Options}}
+		<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+	{{end}}
+	</ul>
+  </body>
+</html>
+`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
+
+func JsonStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
+}
+
+type Option struct {
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
+}
